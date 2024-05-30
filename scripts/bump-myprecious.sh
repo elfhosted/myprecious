4e8811fbdb44eb6cbf4fa84e6d5d266f8c2a915b
David Young <davidy@funkypenguin.co.nz>
2024-05-30 13:24:28 +1200

    fix: more debug
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/bump-myprecious.sh
+++ scripts/bump-myprecious.sh
@@ -2,5 +2,3 @@
 
 NEW_VERSION=$(grep -E "^version" Chart.yaml |sed -r 's/(version: )([0-9]+)\.([0-9]+)\.([0-9]+)(.*)/echo version: \2.\3.$((\4+1))-pre-01/ge')
 sed -iE "s/^version:.*/${NEW_VERSION}/g" Chart.yaml
-
-
