c67d228da92b150ac1815ad9ba88c077a7db7917
David Young <davidy@funkypenguin.co.nz>
2024-10-07 14:08:59 +1300

    permit renovate to bump chart version
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/bump-myprecious.sh
+++ scripts/bump-myprecious.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-NEW_VERSION=$(grep -E "^version" Chart.yaml |sed -r 's/(version: )([0-9]+)\.([0-9]+)\.([0-9]+)(.*)/echo version: \2.\3.$((\4+1))-pre-01/ge')
-sed -iE "s/^version:.*/${NEW_VERSION}/g" Chart.yaml
+NEW_VERSION=$(grep -E "^version" charts/myprecious/Chart.yaml |sed -r 's/(version: )([0-9]+)\.([0-9]+)\.([0-9]+)(.*)/echo version: \2.\3.$((\4+1))-pre-01/ge')
+sed -iE "s/^version:.*/${NEW_VERSION}/g" charts/myprecious/Chart.yaml
